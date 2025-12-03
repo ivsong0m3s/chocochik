e8f3e7a3d2a117ccf53c906243cd8362ff74cb17
ivsong0m3s <143664644+ivsong0m3s@users.noreply.github.com>
2025-12-03 20:00:12 -0300

    atualização

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+**Passo 3: Envie para o GitHub**
+Abra o terminal e rode:
+```bash
+git add .
+git commit -m "Adiciona config do PostCSS"
+git push
